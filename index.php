3f41d755ebc35e049ef5351dde455e2ef5683c5b
dsteve98 <dan.steve2910@gmail.com>
2021-05-19 10:10:24 +0700

    Initial Commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Steve's !</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="assets/css/main.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <script src="main.js"></script>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="wrap h90 bg_bw">
+                <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-center caption">
+                    <Span class="title1">HELLO!</span>
+                </div>
+                <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-center content1">
+                    <p>My name is Steve Daniels. I'm an Informatics graduate. My main interests are data science, artificial intelligence, and web programming.</p>
+                </div>
+                <!-- <div class="col-6"></div> -->
+            </div>
+        </div>
+        <div class="row">
+            <div class="wrap bg_wb text-center" style="font-size:24px">
+                <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-center top20">
+                    <span>Education</span>
+                </div>
+                <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-left fstart top20 bot20">
+                    <img src="./assets/images/logo-its.jpg" style="float: left; width: 15vw; height: auto; margin-right: 20px;">
+                    <span>Bachelor's degree in Informatics Engineering<br>Institut Teknologi Sepuluh Nopember<br><span>GPA: 3.63/4.00</span></span>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="wrap bg_bw txt_white" style="font-size:24px">
+                <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-center top20">
+                    <span>Licenses and Certification</span>
+                </div>
+                <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-left top20">
+                    <ul>
+                        <li>IBM Data Analyst Professional Certificate <br>Issued Jan 2021 | <a href="https://coursera.org/share/e1d560c348e7161aa9adea4956124d06">Credential</a> </li>
+                        <li>Data Science Fundamentals with Python and SQL Specialization <br>Issued Dec 2020 | <a href="https://coursera.org/share/a2c8279956e221def3b2afeee6ae9e8a">Credential</a> </li>
+                        <li>IBM Data Science Professional Certificate <br>Issued Dec 2020 | <a href="https://coursera.org/share/e1d560c348e7161aa9adea4956124d06">Credential</a> </li>
+                        <li>Applied Data Science Specialization <br>Issued Nov 2020 | <a href="https://coursera.org/share/ed92eef403135c36b33df7ae44c41984">Credential</a> </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="wrap bg_wb" style="font-size:24px">
+                <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-center top20">
+                    <span>Work Experience</span>
+                </div>
+                <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-left top20">
+                    <h2>PT. Jaya Sakti Mandiri Unggul</h2>
+                    <ul>
+                        <li>Internship as web application developer</li>
+                        <li>Built web-based inventory management system</li>
+                        <li>Created functions for web-app to make print format report of transactions</li>
+                        <li>Tools : PHP, JavaScript, Bootstrap, MySQL, Laravel</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="wrap bg_bw txt_white" style="font-size:24px">
+                <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-center top20">
+                    <span>Project</span>
+                </div>
+                <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-left top20">
+                    <h2>Face Recognition from IP Camera</h2>
+                    <ul>
+                        <li>Face recognition from IP camera placed in Department</li>
+                        <li>Group Project for Computer Vision Class</li>
+                        <li>Tools : Python (OpenCV, Keras)</li>
+                    </ul>
+                </div>
+                <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-left top20">
+                    <h2>Indonesian Sign Language Recognition</h2>
+                    <ul>
+                        <li>Program to recognize Indonesian sign language into text</li>
+                        <li>Bachelor Final Project</li>
+                        <li>Detection source: image, video (file and webcam)</li>
+                        <li>Presented in ICITDA 2020</li>
+                        <li>Tools : Python (OpenCV, Numpy), Darknet</li>
+                    </ul>
+                </div>
+                <div class="col-md-6 col-md-offset-3 col-sm-6 col-sm-offset-3 text-left top20">
+                    <h2>Chinese Restaurant in Toronto</h2>
+                    <ul>
+                        <li>Program to determine the best place to open an Chinese Restaurant in Toronto</li>
+                        <li>Self-project for completing IBM Professional Data Science Certification</li>
+                        <li>Notebook: <a href="https:/bit.ly/cproTBN">bit.ly/cproTBN</a></li>
+                        <li>Tools : Python (Library: Pandas, Numpy, requests, sklearn, folium, matplotlib). Foursquare API</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
